13eb2dd6d54ba6c1729692e518161c0985ed1c5f
Spencer Markin <markinspencer@gmail.com>
2020-02-09 01:34:24 -0500

    add description

--- CS61A/labs/lab01.scm
+++ CS61A/labs/lab01.scm
@@ -3,6 +3,13 @@
 
 (load "../simply.scm")
 
+;; Question 1:
+;; Write a function that takes a word and outputs the word in pig latin.
+;; Move the beginning of the word up to the first vowel to the end of 
+;; the word and append "ay"
+;; ex.  hello -> ellohay
+;; ex.  program -> ogrampray
+
 (define (pigl wd)
 	(if(pl-done? wd)
 		(word wd 'ay)
